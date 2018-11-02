e8298f3ff5a624384a42802215f11618cd51a0f8
PrincessChalet <chaletshelton@gmail.com>
2018-11-02 13:12:28 -0500

    added Order scaffold

---
+++ app/views/orders/_order.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! order, :id, :order_id, :customer_names, :tables, :created_at, :updated_at
+json.url order_url(order, format: :json)
---
+++ db/migrate/20181102181216_create_orders.rb
@@ -0,0 +1,11 @@
+class CreateOrders < ActiveRecord::Migration[5.2]
+  def change
+    create_table :orders do |t|
+      t.integer :order_id
+      t.string :customer_names
+      t.integer :tables
+
+      t.timestamps
+    end
+  end
+end
